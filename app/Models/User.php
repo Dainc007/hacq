d0788fb5cdd160f4a09db5c95efd1d0344440cdb
Daniel Heinze <danielheinze96@gmail.com>
2025-03-15 12:25:51 +0100

    Konfiguracja Pint, Rector i Providera #1

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -44,5 +46,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isAdmin(): bool
+    {
+        return Auth::user()->id === 1;
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Services\LogService;
+use BezhanSalleh\FilamentShield\FilamentShield;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +27,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $isProduction = $this->app->isProduction();
+
+        Model::shouldBeStrict($isProduction);
+        DB::prohibitDestructiveCommands($isProduction);
+        URL::forceScheme('https');
+
+        date_default_timezone_set(config('app.timezone'));
+
+        Vite::prefetch(concurrency: 3)->useAggressivePrefetching();
+
+        $this->handleGates();
+
+    }
+
+    private function handleGates() : void
+    {
+        Gate::define('viewPulse', function (User $user) {
+            return $user->isAdmin();
+        });
+
+        Gate::guessPolicyNamesUsing(function (string $modelClass) {
+            return str_replace('Models', 'Policies', $modelClass).'Policy';
+        });
+
+        Gate::define('viewPulse', fn (User $user): bool => $user->isAdmin());
     }
 }
